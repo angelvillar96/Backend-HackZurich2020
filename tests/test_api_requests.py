3583ca7ff0076c419cb2e7833370eb2528d79b75
angelvillar96 <angelvillarcorrales@hotmail.com>
2020-09-19 00:11:35 +0200

    Change log:  - Added test directory for unit testing  - Started working on API requests library

---
+++ tests/test_api_requests.py
@@ -0,0 +1,13 @@
+"""
+Testing the functionality of the API Requests processing libraries
+"""
+
+import sys
+
+import api.processing.api_requests as api_requests
+
+
+def test_get_food_name():
+    """
+    Testing that the results of the Bite Food Recognition System works fine
+    """
