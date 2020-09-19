d5761ed6132bc0d9f52d308a517eef21f75d3e82
rwanner <wanner.rafael@icloud.com>
2020-09-19 10:48:06 +0200

    helper function for overview

--- app/processing/utils.py
+++ app/processing/utils.py
@@ -1,5 +1,40 @@
 """
 Methods for data processing and other common functionalities
 """
+from app.models import User, Food
 
-# 
+def process_overview(user, date):
+    foods = user.foods.filter_by(date_consumed=date).all()
+
+    food_list = []
+    total_calories = 0
+    total_fat = 0
+    total_carbs = 0
+    total_protein = 0
+    for food in foods:
+        dict = {
+            'name': food.name,
+            'calories': food.calories,
+            'fat': food.fat,
+            'carbs': food.carbs,
+            'protein': food.protein
+        }
+        food_list.append(dict)
+        total_calories += food.calories
+        total_fat += food.fat
+        total_carbs += food.carbs
+        total_protein += food.protein
+
+    progress = user.calories / total_calories * 100
+
+    payload = {
+        'date_consumed': date,
+        'foods': food_list,
+        'total_calories': total_calories,
+        'total_carbs': total_carbs,
+        'total_fat': total_fat,
+        'total_protein': total_protein,
+        'progress': progress
+    }
+
+    return payload
